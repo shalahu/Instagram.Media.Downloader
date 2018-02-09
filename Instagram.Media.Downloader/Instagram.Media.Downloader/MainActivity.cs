74a893d243dc8a50951a12bd4ce4d38cdbc3fcc7
shalahu <shalahu@163.com>
2018-02-09 22:28:32 +0800

    Basic GUI and mock-up to start

---
+++ Instagram.Media.Downloader/Instagram.Media.Downloader/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Instagram.Media.Downloader
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var mediaUrlEditText = FindViewById<EditText>(Resource.Id.mediaUrl);
+
+            mediaUrlEditText.Click += (sender, args) =>
+            {
+                if (mediaUrlEditText.Text == Resources.GetString(Resource.String.default_media_url))
+                    mediaUrlEditText.Text = string.Empty;
+            };
+
+            var downloadButton = FindViewById<Button>(Resource.Id.download);
+
+            downloadButton.Click += (sender, args) =>
+            {
+                var mediaUrl = mediaUrlEditText.Text;
+                if (!string.IsNullOrEmpty(mediaUrl))
+                {
+
+                }
+                else
+                {
+                    Toast.MakeText(this, "Media url should not be empty.", ToastLength.Long).Show();
+                }
+            };
+        }
+    }
+}
+
